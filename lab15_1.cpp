b2da88dd39a479931f14d48df134640a093e1808
touch <tttouch1122@gmail.com>
2023-01-17 16:47:04 +0700

    15.1

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+  for (int i = 1; i < N; i++) {
+    T key = d[i];
+    int j = i - 1;
 
+    while (key > d[j] && j >= 0) {
+      d[j + 1] = d[j];
+      --j;
+    }
+    d[j + 1] = key;
+    cout << "Pass "<<i<<":";
+    for(int i = 0;i < N;i++){
+        cout << d[i] << " ";
+    }
+    cout << "\n";
+  }
+  
+  
 }
 
 int main(){
