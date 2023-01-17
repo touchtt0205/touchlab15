fa3e8baa9aa75112457111bd4aec172dba981617
touch <tttouch1122@gmail.com>
2023-01-17 16:52:10 +0700

    15.2

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,41 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool A[][N]){
+for(int i = 0; i < N ; i++){
+    for(int j = 0; j < N ; j++){
+        if(A[i][j] == 0) cout << "0" << " ";
+        else cout << "1" << " ";
+    }
+    cout << endl;
+}
+}
+
+void inputMatrix(double B[][N]){
+for(int i = 0; i < N ; i++){
+    cout << "Row " << i+1 << ": ";
+    for(int j = 0; j < N ; j++){
+        cin >> B[i][j];
+        
+    }
+}
+}
+
+
+void findLocalMax(const double G[][N], bool F[][N]){
+for(int i = 0; i < N ; i++){
+    for(int j = 0; j < N ; j++){
+        F[i][j] = 0;
+    }
+}
+for(int i = 1; i < N-1 ; i++){
+    for(int j = 1; j < N-1 ; j++){
+    if(G[i][j] >= G[i-1][j] and G[i][j] >= G[i][j-1] and G[i][j] >= G[i+1][j] and G[i][j] >= G[i][j+1]){
+    F[i][j] = 1;
+    }
+    else F[i][j] = 0;
+}
+}
+
+}
